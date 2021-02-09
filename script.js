5af6d4098eff1bd531b26112bf21060cfbdf0f20
kdotlilla <kdotlilla@gmail.com>
2021-02-09 09:50:52 +0100

    Built the functionality of the game

--- script.js
+++ script.js
@@ -4,3 +4,41 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+const generateTarget = () => {
+    const randomNumber = Math.floor((Math.random() * 10));
+    return randomNumber;
+}
+
+const compareGuesses = (userGuess,computerGuess,targetNumber) => {
+    targetNumber = generateTarget();
+    if (userGuess === computerGuess) {
+        return true;
+    }
+    else if (Math.abs(targetNumber - userGuess) > Math.abs(targetNumber - computerGuess)) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+const updateScore = winner => {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    if (winner === 'human') {
+        humanScore++;
+    }
+    else if (winner = 'computer') {
+        computerScore++;
+    }
+}
+
+const advanceRound = () => {
+    let currentRoundNumber;
+    currentRoundNumber++;
+
+}
+
+
+
